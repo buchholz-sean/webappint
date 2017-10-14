fd8bdebdd2e8f88c1183de967e95942b4368e212
Sean Buchholz <smbuchholz@fullsail.edu>
2017-10-14 18:16:45 -0400

    Extract database setup to its own file

---
+++ bullets/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Configure Firebase
+import * as firebase from 'firebase';
+const config = {
+  apiKey: "AIzaSyA1_MLFnsq5VECmJnFo1d7JJLl7pMN6DE0",
+  authDomain: "bullets-d19a6.firebaseapp.com",
+  databaseURL: "https://bullets-d19a6.firebaseio.com",
+  storageBucket: "bullets-d19a6.appspot.com",
+};
+export default firebaseApp = firebase.initializeApp(config);
