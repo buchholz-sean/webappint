2b575ef07872023eab90a4c472ed8180240a803b
Sean Buchholz <smbuchholz@fullsail.edu>
2017-10-16 10:46:32 -0400

    - Updated navigation options for React Navigation tab bar - Whitespace changes

--- bullets/pages/Monthly.js
+++ bullets/pages/Monthly.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { Header, Body, Title, Content } from 'native-base';
+import {StyleSheet, View, Text, Image} from 'react-native';
+import {Header, Body, Title, Content} from 'native-base';
 
 // Global styles
 import styles from '../styles/global';
 
 export default class MonthlyScreen extends React.Component {
     static navigationOptions = {
-        title: 'Monthly',
+        tabBarLabel: 'Monthly Log',
+        tabBarIcon: ({tintColor}) => (<Image source={require('../assets/basic_elaboration_calendar_empty.png')} style={[
+            styles.tabIcon, {
+                tintColor: tintColor
+            }
+        ]}/>)
     };
     render() {
         return (
             <View style={styles.container}>
                 <Header>
                     <Body>
-                        <Title>Monthly</Title>
+                        <Title>Monthly Log</Title>
                     </Body>
                 </Header>
                 <Content>
