7dca76aa5c342bcc89a812f2aa0651a11489ae10
Sean Buchholz <smbuchholz@fullsail.edu>
2017-10-16 12:45:31 -0400

    - Cleaned up React Native and NativeBase imports - Updated navigation options for React Navigation tab bar - Blank entry validation - Added Cancel button to Add Entry prompt - Items array now renders in reverse chron order - Entries now rendered as NativeBase List Component instead of Card List - SubmitButton component no longer inside Footer Component - Added/improved commenting throughout - Whitespace changes

--- bullets/pages/Daily.js
+++ bullets/pages/Daily.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { StyleSheet, View, TextInput, Text, ListView, AlertIOS, FlatList } from 'react-native';
-import { Header, Body, Title, Content, Footer, Icon, List, Card, ActionSheet } from 'native-base';
+import {StyleSheet, View, AlertIOS, Image} from 'react-native';
+import {
+    Header,
+    Body,
+    Title,
+    Content,
+    List,
+    ActionSheet,
+    Text
+} from 'native-base';
 
 // Import momentJS for pretty Date/Time objects
 import moment from 'moment';
 
 // Import custom Components
-const ListItem = require('../components/ListItem');
+const ListEntry = require('../components/ListEntry');
 const SubmitButton = require('../components/SubmitButton');
 
 // Global styles
@@ -16,119 +24,105 @@ import firebaseApp from '../firebaseConfig';
 
 export default class DailyScreen extends React.Component {
 
+    // Options for Tab Navigation
     static navigationOptions = {
         tabBarLabel: 'Daily Log',
-        tabBarIcon: ({ tintColor }) => (
-        <Icon name='ios-calendar-outline' style={[styles.tabIcon, {color: tintColor}]} />
-        ),
+        tabBarIcon: ({tintColor}) => (<Image source={require('../assets/basic_spread_text_bookmark.png')} style={[
+            styles.tabIcon, {
+                tintColor: tintColor
+            }
+        ]}/>)
     };
 
     constructor(props) {
         super(props);
-        // State of data source
         this.state = {
-            dataSource: new ListView.DataSource({
-                rowHasChanged: (row1, row2) => row1 !== row2
-            }),
             items: []
         };
         // Database reference
         this.itemsRef = firebaseApp.database().ref();
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.watchForItems(this.itemsRef);
     }
 
     _renderItem(item) {
         const onPress = () => {
-            AlertIOS.prompt(
-                'Clear Item',
-                null,
-                [
+            // IOS prompt to mark Task entries complete (remove from list)
+            // TODO: Provide 'Remove' functionality for Notes/Events
+            // TODO: Provide visual feedback on complete items instead of remove
+            AlertIOS.prompt('Mark Item Complete?', null, [
+                {
+                    text: 'Complete',
+                    onPress: (text) => this.itemsRef.child(item._key).remove()
+                }, {
+                    text: 'Cancel',
+                    onPress: (text) => console.log('Canceled')
+                }
+            ], 'default')
+        }
+        return (<ListEntry item={item} onPress={onPress}/>);
+    }
+
+    _addItem() {
+        // Configuration for ActionSheet
+        var BUTTONS = ['Task', 'Event', 'Note', 'Cancel'];
+        var CANCELINDEX = 3;
+        ActionSheet.show({
+            options: BUTTONS,
+            cancelButtonIndex: CANCELINDEX,
+            title: 'Entry Type'
+        }, buttonIndex => {
+            // If user selects option other than Cancel
+            if (buttonIndex != CANCELINDEX) {
+                // IOS prompt to add new entry of selected type
+                AlertIOS.prompt('New ' + BUTTONS[buttonIndex], null, [
                     {
-                        text: 'Remove',
-                        onPress: (text) => this.itemsRef.child(item._key).remove()
-                    },
-                    {
+                        text: 'Add',
+                        onPress: (text) => {
+                            // If prompt text input is not blank
+                            if (text.trim() != '') {
+                                // Push item to front database
+                                this.itemsRef.push({title: text, date: moment().format('MMDDYY'), completed: false, entryType: BUTTONS[buttonIndex]})
+                            }
+                        }
+                    }, {
                         text: 'Cancel',
                         onPress: (text) => console.log('Canceled')
                     }
-                ],
-                'default'
-            )}
-        return(
-            <ListItem item={item} onPress={onPress} />
-        );
-    }
-
-    _addItem(){
-        console.log('Add Entry pressed');
-        var BUTTONS = ['Task', 'Event', 'Note', 'Cancel'];
-        var CANCELINDEX = 3;
-        ActionSheet.show(
-            {
-                options: BUTTONS,
-                cancelButtonIndex: CANCELINDEX,
-                title: 'Entry Type'
-            },
-            buttonIndex => {
-                AlertIOS.prompt(
-                    'New item',
-                    null,
-                    [
-                        {
-                            text: 'Add',
-                            onPress: (text) => {
-                                this.itemsRef.push({
-                                    title: text,
-                                    date: moment().format('MMDDYY'),
-                                    completed: false,
-                                    entryType: BUTTONS[buttonIndex]
-                                })
-                            }
-                        }
-                    ],
-                    'plain-text'
-                );
+                ], 'plain-text');
             }
-        );
+        });
     }
 
-    watchForItems(itemsRef){
+    watchForItems(itemsRef) {
         // Listen for changes to database
         itemsRef.on('value', (snapshot) => {
             var items = [];
-            // Push each item to items array
+            // Push each item to front of items array
             snapshot.forEach((child) => {
-                items.push({
-                    title: child.val().title,
-                    date: child.val().date,
-                    entryType: child.val().entryType,
-                    _key: child.key
-                });
+                items.unshift({title: child.val().title, date: child.val().date, entryType: child.val().entryType, _key: child.key});
             });
             this.setState({
-                // Update db with items array
-                dataSource: this.state.dataSource.cloneWithRows(items),
+                // Update state with entries
                 items: items
             });
         });
     }
+
     render() {
         return (
             <View style={styles.container}>
                 <Header>
                     <Body>
-                        <Title>Daily</Title>
+                        <Title>Daily Log</Title>
                     </Body>
                 </Header>
                 <Content>
-                    <Card dataArray={this.state.items} renderRow={(item) => this._renderItem(item)} style={styles.cardList}></Card>
+                    <List dataArray={this.state.items} renderRow={(item) => this._renderItem(item)}/>
                 </Content>
-                <Footer>
-                    <SubmitButton title='Add Entry' onpress={this._addItem.bind(this)} />
-                </Footer>
+                <SubmitButton title='Add Entry' onpress={this._addItem.bind(this)}/>
             </View>
         )
     }
