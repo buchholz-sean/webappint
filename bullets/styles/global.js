dfe380c3d654ddae0d10376315990c5b6e84fb7b
Sean Buchholz <smbuchholz@fullsail.edu>
2017-10-16 12:46:10 -0400

    General style updates

--- bullets/styles/global.js
+++ bullets/styles/global.js
@@ -1,27 +1,31 @@
 import react from 'react';
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  cardList: {
-      alignSelf: 'stretch',
-  },
-  tabIcon: {
-      color: '#3b5998',
-  },
-  listIcon: {
-      width: 26,
-      height: 26,
-      marginRight: 16
-  },
-  listText: {
-      fontSize: 18
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center'
+    },
+    tabIcon: {
+        width: 26,
+        height: 26,
+        tintColor: '#3b5998'
+    },
+    submitButton: {
+        backgroundColor: '#3b5998'
+    },
+    listEntry: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start'
+    },
+    listIcon: {
+        width: 26,
+        height: 26
+    },
+    listText: {
+        fontSize: 18
+    }
 });
 
 export default styles;
