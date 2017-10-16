7366a0da4543d6b6b78faa8ce9f80667e6812e63
Sean Buchholz <smbuchholz@fullsail.edu>
2017-10-16 10:44:04 -0400

    - Removed superfluous imports - Whitespace changes

--- bullets/App.js
+++ bullets/App.js
@@ -1,29 +1,31 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Root } from 'native-base';
-import { TabNavigator } from 'react-navigation';
+import {Root} from 'native-base';
+import {TabNavigator} from 'react-navigation';
 
 // Import screens for navigation
 import DailyScreen from './pages/Daily';
 import MonthlyScreen from './pages/Monthly';
 import FutureScreen from './pages/Future';
 
-// Global styles
-import styles from './styles/global';
-
 const Screens = TabNavigator({
-    Daily: {screen: DailyScreen},
-    Monthly: {screen: MonthlyScreen},
-    Future: {screen: FutureScreen}
-    }, {
+    Daily: {
+        screen: DailyScreen
+    },
+    Monthly: {
+        screen: MonthlyScreen
+    },
+    Future: {
+        screen: FutureScreen
+    }
+}, {
     tabBarOptions: {
         activeTintColor: '#3b5998',
         labelStyle: {
-            fontSize: 12,
+            fontSize: 12
         },
         style: {
-            backgroundColor: '#fff',
-        },
+            backgroundColor: '#fff'
+        }
     }
 });
 
@@ -34,7 +36,7 @@ export default class App extends React.Component {
     render() {
         return (
             <Root>
-                <Screens />
+                <Screens/>
             </Root>
         );
     }
