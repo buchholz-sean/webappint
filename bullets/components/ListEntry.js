c9f9976af9b6f8e4e8b67f16d7820e118df5da07
Sean Buchholz <smbuchholz@fullsail.edu>
2017-10-16 10:45:28 -0400

    Renamed component for better in-app continuity

---
+++ bullets/components/ListEntry.js
@@ -0,0 +1,51 @@
+'use strict';
+
+import React, {Component} from 'react';
+import {StyleSheet, Image, View} from 'react-native';
+import {ListItem, Icon, Left, Text} from 'native-base';
+
+import styles from '../styles/global';
+
+class ListEntry extends React.Component {
+
+    renderImage(entryType) {
+        // Render correct entry icon for corresponding entry types
+        switch (entryType) {
+            // Cases provided with index and name of entry types (just in case)
+            case 0:
+            case 'Task':
+            // TODO: Render Complete vs. Incomplete tasks with visual feedback/different icon?
+                return <Image source={require('../assets/arrows_check.png')} style={styles.listIcon}/>;
+                break;
+            case 1:
+            case 'Event':
+                return <Image source={require('../assets/basic_bookmark.png')} style={styles.listIcon}/>
+                break;
+            case 2:
+            case 'Note':
+                return <Image source={require('../assets/basic_book_pen.png')} style={styles.listIcon}/>
+                break;
+            default:
+                return <Image source={require('../assets/arrows_check.png')} style={styles.listIcon}/>
+        }
+    }
+
+    render() {
+        const contents = this.props.item.entryType == 0 || this.props.item.entryType == 'Task'
+            ? <ListItem icon button onPress={this.props.onPress} style={styles.listEntry}>
+                    <Left>{this.renderImage(this.props.item.entryType)}</Left>
+                    <Text style={styles.listText}>{this.props.item.title}</Text>
+                </ListItem>
+            : <ListItem icon style={styles.listEntry}>
+                <Left>{this.renderImage(this.props.item.entryType)}</Left>
+                <Text style={styles.listText}>{this.props.item.title}</Text>
+            </ListItem>
+        return (
+            <View>
+                {contents}
+            </View>
+        );
+    }
+}
+
+module.exports = ListEntry;
