4fd426543b8c2f5ec73251fd5ccebc03c641905f
Sean Buchholz <smbuchholz@fullsail.edu>
2017-10-19 10:05:41 -0400

    - Updated with color variables - Shifted Component-only styles to inline - Standardized some method names

--- bullets/components/SubmitButton.js
+++ bullets/components/SubmitButton.js
@@ -1,20 +1,26 @@
 'use strict';
 
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {Button, Text, Icon} from 'native-base';
 
-import styles from '../styles/global';
+import colors from '../styles/variables';
 
 class SubmitButton extends React.Component {
     render() {
         return (
-            <Button full iconRight onPress={this.props.onpress} style={styles.submitButton}>
+            <Button full iconRight onPress={this.props.onPress} style={styles.submitButton}>
                 <Text>{this.props.title}</Text>
                 <Icon name='ios-create-outline'/>
             </Button>
         );
     }
 }
+
+var styles = StyleSheet.create({
+    submitButton: {
+        backgroundColor: colors.buttonColor
+    },
+});
 
 module.exports = SubmitButton;
