a0d4b438c285df092ad4f0bd6e13b508df1f6a65
Sean Buchholz <smbuchholz@fullsail.edu>
2017-10-20 07:33:32 -0400

    Class name change for consistency through app

--- bullets/pages/Future.js
+++ bullets/pages/Future.js
@@ -5,7 +5,7 @@ import {Header, Body, Title, Content, Container} from 'native-base';
 // Global styles
 import styles from '../styles/global';
 
-export default class Future extends React.Component {
+export default class FutureScreen extends React.Component {
     static navigationOptions = {
         tabBarLabel: 'Future Log',
         tabBarIcon: ({tintColor}) => (<Image source={require('../assets/basic_elaboration_calendar_next.png')} style={[
